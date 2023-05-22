0eeb388bbb9c6727ef151a977b694ee02a50eb98
Rif77777 <130090788+Rif77777@users.noreply.github.com>
2023-05-23 00:19:41 +0200

    up

---
+++ js/practice.js
@@ -0,0 +1,24 @@
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+
+//   // Change code above this line
+
+
+//   if(totalPrice >= customerCredits) {
+//     message = "Insufficient funds!"
+//   }
+//   else(totalPrice - customerCredits) {
+//     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`
+//   }
+//   return message;
+  
+// }
+
+// makeTransaction(3000, 5, 23000);
+// makeTransaction(1000, 3, 15000);
+// makeTransaction(5000, 10, 8000);
+// makeTransaction(2000, 8, 10000);
+// makeTransaction(500, 10, 5000);
+
